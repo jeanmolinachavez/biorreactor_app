48221b43a3bdef23e3574f50d86d91fcdf16c2a2
jeanmolinachavez <jean.molina@alumnos.ucn.cl>
2025-05-20 16:09:52 -0400

    quitar la funcion watchdog_wifi

--- run.py
+++ run.py
@@ -1,9 +1,6 @@
 from app import create_app
-from watchdog_wifi import iniciar_watchdog
 
 app = create_app()
-
-iniciar_watchdog() # Iniciar el monitoreo de Wifi
 
 if __name__ == '__main__':
     app.run(debug=True)
